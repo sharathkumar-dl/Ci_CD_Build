31197c384ff175967790739a2a13089ff789ac9f
sharathkumar-dl <sharusharath343@gmail.com>
2026-08-15 00:08:18 +0530

    aading new code to test ci cd

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CI_CD_build_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
